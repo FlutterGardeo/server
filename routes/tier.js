2e9afe332c462c7ca4fe01369566390f073ef2c2
GimhanRanasinghe <gimhanravi@gmail.com>
2023-03-31 07:23:03 +0530

    checkout error corrected

--- routes/tier.js
+++ routes/tier.js
@@ -5,14 +5,15 @@ const Service = require("../models/service");
 
 const router = express.Router();
 
-router.post("/checkout", auth, async (req, res) => {
+router.post("/checkout",async (req, res) => {
   console.log('received checkout request');
-  const userId = req.userId;
+  const userId = req.body.userId;
   const total = req.body.total;
 
   try {
     console.log('deleteing');
-    const cart = await Cart.findByIdAndDelete({ user: userId })
+    const cart = await Cart.findOneAndDelete({ user: userId })
+    res.send(cart)
 
   } catch (error) {
     res.status(400).send(error);
